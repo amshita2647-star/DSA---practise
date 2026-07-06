3f63ef0ecd647824f28b83042e02353c74e5a193
amshita2647-star <amshita2647@gmail.com>
2026-07-06 22:48:59 +0530

    Implement pattern18 and pattern19 functions
    Added functions for alpha triangle and symmetric-void patterns.

--- pat.cpp
+++ pat.cpp
@@ -23,6 +23,57 @@ void pattern17(int n)
         cout<<endl;
     }
 }
+void pattern18(int n) //alpha triangle pattern//
+{
+    char a= 'A'+n-1;
+    for(int i=0;i<n;i++)
+    {
+        for(char b=a-i;b<=a;b++)
+        {
+            cout<<b;
+        }
+        cout<<endl;
+    }
+}
+void pattern19(int n) //Symmetric-Void Pattern//
+{
+    int initial_space=0;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=1;j<=n-i;j++)
+        {
+            cout<<"*";
+        }
+        for(int j=0;j<initial_space;j++)
+        {
+            cout<<" ";
+        }
+        initial_space+=2;
+        for(int j=1;j<=n-i;j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    initial_space=2*(n-1);
+    for(int i=n-1;i>=0;i--)
+    {
+        for(int j=1;j<=n-i;j++)
+        {
+            cout<<"*";
+        }
+        for(int j=0;j<initial_space;j++)
+        {
+            cout<<" ";
+        }
+        initial_space-=2;
+        for(int j=1;j<=n-i;j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
 int main()
 {
     pattern17(5);
