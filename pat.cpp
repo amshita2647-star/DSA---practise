82cd29ecc1ba98b279dce69e3fdc4825ca188a44
amshita2647-star <amshita2647@gmail.com>
2026-07-06 23:22:35 +0530

    Implement symmetric butterfly pattern function
    Added a function to print a symmetric butterfly pattern based on the input size.

--- pat.cpp
+++ pat.cpp
@@ -74,6 +74,46 @@ void pattern19(int n) //Symmetric-Void Pattern//
         cout<<endl;
     }
 }
+void pattern20(int n) //symmetric-Butterfly pattern//
+{
+    int initial_space=2*(n-1);
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<=i;j++)
+        {
+            cout<<"*";
+        }
+        for(int j=0;j<initial_space;j++)
+        {
+            cout<<" ";
+        }
+        initial_space-=2;
+        for(int j=0;j<=i;j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    initial_space=2;
+    for(int i=n-2;i>=0;i--)
+    {
+        
+        for(int j=0;j<=i;j++)
+        {
+            cout<<"*";
+        } 
+        for(int j=0;j<initial_space;j++)
+        {
+            cout<<" ";
+        }
+        initial_space+=2;
+        for(int j=0;j<=i;j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
 int main()
 {
     pattern17(5);
